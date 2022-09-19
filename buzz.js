5d331a39a45e6f874cf2c42007c55abd9a20b57e
Dylan Lewis <103405022+BubbleManster@users.noreply.github.com>
2022-09-19 07:03:06 +0100

    Update buzz.js

--- buzz.js
+++ buzz.js
@@ -1,35 +1,51 @@
-const enter = new KeyboardEvent('keydown', {altKey:false,
-    bubbles: true,
-    cancelBubble: false, 
-    cancelable: true,
-    charCode: 0,
-    code: "Enter",
-    composed: true,
-    ctrlKey: false,
-    currentTarget: null,
-    defaultPrevented: true,
-    detail: 0,
-    eventPhase: 0,
-    isComposing: false,
-    isTrusted: true,
-    key: "Enter",
-    keyCode: 13,
-    location: 0,
-    metaKey: false,
-    repeat: false,
-    returnValue: false,
-    shiftKey: false,
-    type: "keydown",
-    which: 13});
+const enter = new KeyboardEvent("keydown", {
+  altKey: false,
+  bubbles: true,
+  cancelBubble: false,
+  cancelable: true,
+  charCode: 0,
+  code: "Enter",
+  composed: true,
+  ctrlKey: false,
+  currentTarget: null,
+  defaultPrevented: true,
+  detail: 0,
+  eventPhase: 0,
+  isComposing: false,
+  isTrusted: true,
+  key: "Enter",
+  keyCode: 13,
+  location: 0,
+  metaKey: false,
+  repeat: false,
+  returnValue: false,
+  shiftKey: false,
+  type: "keydown",
+  which: 13,
+});
 
-for(let i = 0; i < gameData.today.answers.length; i++){
-  
-  for(let j = 0; j < gameData.today.answers[i].split("").length; j++){
-      // Type the letter gameData.today.answers[i].split("")[j]
-      window.dispatchEvent(new KeyboardEvent('keydown', {
-          'key': `${gameData.today.answers[i].split("")[j]}`
-      }));
-      setTimeout(() => console.log("Wait"), 1000);
-  }
-  document.getElementsByClassName("hive-action hive-action__submit sb-touch-button")[0].dispatchEvent(enter);
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+async function typeWords() {
+  for (let i = 0; i < gameData.today.answers.length; i++) {
+    for (let j = 0; j < gameData.today.answers[i].split("").length; j++) {
+      // Type the letter gameData.today.answers[i].split("")[j]
+      window.dispatchEvent(
+        new KeyboardEvent("keydown", {
+          key: `${gameData.today.answers[i].split("")[j]}`,
+        })
+      );
+      await sleep(500).then(() => {
+        console.log("Wait");
+      });
+    }
+    document
+      .getElementsByClassName(
+        "hive-action hive-action__submit sb-touch-button"
+      )[0]
+      .dispatchEvent(enter);
+  }
+}
+typeWords();
